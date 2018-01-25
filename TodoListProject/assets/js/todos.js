2b794b7fc8d57197c622ae87ded1d7312346ecd0
NickWhoCodes <nicholasmprieto@live.com>
2018-01-25 18:34:28 -0500

    Added CSS Properties

--- TodoListProject/assets/js/todos.js
+++ TodoListProject/assets/js/todos.js
@@ -1,10 +1,10 @@
 //Toggle todo item completed
-$("li").on("click", function(){
+$("ul").on("click","li", function(){
   $(this).toggleClass("completed");
 });
 
 //Click on X to delete Todo item
-$("span").click(function(event){
+$("ul").on("click","span", function(){
   $(this).parent().fadeOut(350, function(){
     $(this).remove();
   });
@@ -16,6 +16,10 @@ $("input[type='text']").keypress(function(event){
   if(event.which === 13){
     var newTodo = $(this).val();
     $(this).val("");
-    $("ul").append("<li><span>X</span> " + newTodo + "</li>");
+    $("ul").append("<li><span><i class='far fa-trash-alt'></i></span> " + newTodo + "</li>");
   }
+});
+
+$(".fa-coffee").click(function(){
+	$("input[type='text']").fadeToggle();
 });
