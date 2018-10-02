3cf4bc3c6de836b05279ded479cc2484cde97e0d
Nicholas Prieto <nicholasmprieto@gmail.com>
2018-10-01 21:27:26 -0400

    Cleaning up by removing files that are too basic and added a new folder for Node

--- functions-PS.js
+++
@@ -1,7 +0,0 @@
-function kebabToSnake(sent) {
-  newSent = sent.replace(/-/g, "_");
-  console.log(newSent);
-}
-kebabToSnake("hello-world");
-kebabToSnake("dogs-are-awesome");
-kebabToSnake("blah");
--- jsExercise.js
+++
@@ -1,6 +0,0 @@
-var firstName = prompt("Hello! What is your first name?");
-var lastName = prompt("Awesome, how about your last name?");
-var age = prompt("One last thing, what is your age?");
-alert("Nice to meet you " + firstName + " " + lastName + ", who happens to be "
-+ age + " years old!");
-console.log("Yes, I know all about you..."+ firstName + " " + lastName);
