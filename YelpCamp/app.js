0ff75f662c44d06b99bebc9a9c511857a552154e
Nicholas Prieto <nicholasmprieto@gmail.com>
2018-10-03 15:18:36 -0400

    fixed a syntax error

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -1,9 +1,9 @@
 var express     = require('express'),
     app         = express(),
     bodyParser  = require('body-parser'),
-    mongoose    = require('mongoose')
+    mongoose    = require('mongoose');
 
-mongoose.connect('mongodb://locahost:27017/yelp_camp', { useNewUrlParser: true });
+mongoose.connect('mongodb://localhost:27017/yelp_camp', { useNewUrlParser: true });
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -17,8 +17,8 @@ var Campground = mongoose.model('Campground', campgroundSchema);
 
 Campground.create(
     {
-        name: 'Salmon Creek',
-        image: 'https://images.unsplash.com/photo-1513309642960-b1d95a1b69f7?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=837f10a5b40b69c46e969d8ce31ee473&auto=format&fit=crop&w=500&q=60'
+        name: 'Yellowstone',
+        image: 'https://images.unsplash.com/photo-1524648365428-6bbe1d37c18a?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=616d02546a6e31e2db0cad32b9019190&auto=format&fit=crop&w=500&q=60'
 
     }, function (err, campground) {
         if (err) {
