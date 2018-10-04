1331b58b54d22de21118044df28708b342a66763
Nicholas Prieto <nicholasmprieto@gmail.com>
2018-10-04 14:54:18 -0400

    added semantic ui

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -10,52 +10,76 @@ app.use(bodyParser.urlencoded({extended: true}));
 // Schema Setup
 var campgroundSchema = new mongoose.Schema({
     name: String,
-    image: String
+    image: String,
+    description: String
 });
 
 var Campground = mongoose.model('Campground', campgroundSchema);
 
-Campground.create(
-    {
-        name: 'Yellowstone',
-        image: 'https://images.unsplash.com/photo-1524648365428-6bbe1d37c18a?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=616d02546a6e31e2db0cad32b9019190&auto=format&fit=crop&w=500&q=60'
+// Campground.create(
+//     {
+//         name: 'Yellowstone',
+//         image: 'https://images.unsplash.com/photo-1524648365428-6bbe1d37c18a?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=616d02546a6e31e2db0cad32b9019190&auto=format&fit=crop&w=500&q=60',
+//         description: "It's as if nature has been untouched for thousands of years!"
 
-    }, function (err, campground) {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log("Created a new campground!");
-            console.log(campground);
-        }
-    }
-)
-
-var campgrounds = [
-    { name: 'Salmon Creek', image: 'https://images.unsplash.com/photo-1513309642960-b1d95a1b69f7?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=837f10a5b40b69c46e969d8ce31ee473&auto=format&fit=crop&w=500&q=60' },
-    { name: 'Yellowstone', image: 'https://images.unsplash.com/photo-1524648365428-6bbe1d37c18a?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=616d02546a6e31e2db0cad32b9019190&auto=format&fit=crop&w=500&q=60' },
-    { name: 'Grand Canyon', image: 'https://images.unsplash.com/photo-1535515505622-7621ebc4fc58?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a0e9ad1c57a31b2bc50d57b4f7abf985&auto=format&fit=crop&w=500&q=60' }
-];
+//     }, function (err, campground) {
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             console.log("Created a new campground!");
+//             console.log(campground);
+//         }
+//     }
+// )
 
 app.get('/', function(req, res) {
     res.render('landing');
 })
 
+// INDEX - list of all campgrounds
 app.get('/campgrounds', function(req, res) {
-    res.render('campgrounds', {campgrounds});
+    // Get all campgrounds from db
+    Campground.find({}, function(err, campgrounds) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('index', {campgrounds});
+        }
+    })
+    // res.render('campgrounds', {campgrounds});
 })
 
-app.get('/campgrounds/new', function(req, res) {
-    res.render('new');
-})
-
+// CREATE - create new campground
 app.post('/campgrounds', function(req, res) {
     // get data from form and add to campgrounds array
     var name = req.body.name;
     var image = req.body.image;
-    var newCampground = {name, image};
-    campgrounds.push(newCampground);
-    //redirect back to campgrounds page
-    res.redirect('/campgrounds');
+    var description = req.body.description;
+    var newCampground = {name, image, description};
+    // Create new campground and save to db
+    Campground.create(newCampground, function (err, campground) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.redirect('/campgrounds');
+        }
+    })
+})
+
+// NEW - form for new campground
+app.get('/campgrounds/new', function(req, res) {
+    res.render('new');
+})
+
+// SHOW - shows more info about one campground
+app.get('/campgrounds/:id', function(req, res) {
+    Campground.findById(req.params.id, function(err, found) {
+        if(err) {
+            console.log(err);
+        } else {
+            res.render('show', {campground: found});
+        }
+    })
 })
 
 app.listen(3000, function() {
