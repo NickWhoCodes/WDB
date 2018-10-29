83d0bf70710dfd716c4518b67cf601d4988d17fb
Nicholas Prieto <nicholasmprieto@gmail.com>
2018-10-28 20:06:43 -0400

    fixed javascript styling

--- TodoListProject/assets/js/todos.js
+++ TodoListProject/assets/js/todos.js
@@ -1,25 +1,25 @@
 //Toggle todo item completed
-$("ul").on("click","li", function(){
-  $(this).toggleClass("completed");
-});
+$('ul').on('click','li', function() {
+    $(this).toggleClass('completed')
+})
 
-$(".fa-coffee").click(function(){
-	$("input[type='text']").fadeToggle();
-});
+$('.fa-coffee').click(function() {
+    $("input[type='text']").fadeToggle()
+})
 
 //Click on X to delete Todo item
-$("ul").on("click","span", function(){
-  $(this).parent().fadeOut(350, function(){
-    $(this).remove();
-  });
-  event.stopPropagation();
-});
+$('ul').on('click','span', function() {
+    $(this).parent().fadeOut(350, function() {
+        $(this).remove()
+    })
+    event.stopPropagation()
+})
 
 //Add new Todo item
-$("input[type='text']").keypress(function(event){
-  if(event.which === 13){
-    var newTodo = $(this).val();
-    $(this).val("");
-    $("ul").append("<li><span><i class='far fa-trash-alt'></i></span> " + newTodo + "</li>");
-  }
-});
+$("input[type='text']").keypress(function(event) {
+    if (event.which === 13) {
+        var newTodo = $(this).val()
+        $(this).val('')
+        $('ul').append("<li><span><i class='far fa-trash-alt'></i></span> " + newTodo + '</li>')
+    }
+})
